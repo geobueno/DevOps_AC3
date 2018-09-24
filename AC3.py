82483feac8650b6a1c84c9f10f2802f9f849d711
Georgete Bueno <geobueno@gmail.com>
2018-09-23 21:48:49 -0300

    third commit

--- AC3.py
+++ AC3.py
@@ -1,5 +1,5 @@
 nomes = ['Pedro', 'João', 'Leticia']
-2 for n in nomes:
-3      print(n)
-4 else:
-5      print(“Todos os nomes foram listados com sucesso”)
+for n in nomes:
+  print(n)
+else:
+  print("Todos os nomes foram listados com sucesso")
